c630b6e6929d95b8b8bcf6c11704731d32db95e3
Kkhludneva <e.khludneva@mail.ru>
2024-01-27 23:02:48 +0300

    train stage init

--- load_and_prepare.py
+++ load_and_prepare.py
@@ -3,6 +3,7 @@ import random
 import re
 import sys
 import xml.etree.ElementTree
+import numpy as np
 import pandas as pd
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
@@ -19,30 +20,19 @@ def main():
     seed = params["seed"]
 
     iris = datasets.load_iris()
-    df = pd.DataFrame(iris.data, columns = iris.feature_names)
-
+    df = pd.DataFrame(data= np.c_[iris['data'], iris['target']],
+                      columns= iris['feature_names'] + ['target'])
+    print(df.shape)
     le = LabelEncoder()
-    df['target'] = le.fit_transform(iris.target_names[iris.target])
+    df['target'] = le.fit_transform(df['target'])
 
     train, test = train_test_split(df, test_size=split, random_state=seed)
+    print(train.shape, test.shape)
 
-    output_train = os.path.join("data", "prepared", "train.csv")
-    output_test = os.path.join("data", "prepared", "test.csv")
-
-    print(output_train, output_test, sep='\n')
-
-    fd_out_train = open(output_train, "w", encoding="utf-8")
-    fd_out_test = open(output_test, "w", encoding="utf-8")
-
-    fd_out_train.write(train.to_csv())
-    fd_out_test.write(test.to_csv())
-
-    fd_out_train.close()
-    fd_out_test.close()
+    train.to_csv("data/prepared/train.csv")
+    test.to_csv("data/prepared/test.csv")
 
 
 if __name__ == "__main__":
      main()
 
-
-#%%
---
+++ train.py
@@ -0,0 +1,90 @@
+# import os
+# import pickle
+# import sys
+# import csv
+import statistics
+import torch
+import torch.nn as nn
+import pandas as pd
+from torch.utils.data import DataLoader, Dataset
+from sklearn.metrics import accuracy_score
+import yaml
+from dvclive import Live
+
+class IrisDatatset(Dataset):
+
+    def __init__(self,file_name):
+        price_df=pd.read_csv(file_name)
+
+        x=price_df.iloc[:,0:5].values
+        y=price_df.iloc[:,5].values
+
+        self.x_train=torch.tensor(x,dtype=torch.float32)
+        self.y_train=torch.tensor(y,dtype=torch.float32)
+
+    def __len__(self):
+        return len(self.y_train)
+
+    def __getitem__(self,idx):
+        return self.x_train[idx],self.y_train[idx]
+
+def train(model, train_loader, optimizer, criterion, n_epochs):
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model.to(device)
+    for epoch in range(n_epochs):
+        model.train()
+        losses = []
+        accs = []
+        for batch in train_loader:
+            X_batch = batch[0]
+            y_batch = batch[1]
+            optimizer.zero_grad()
+            pred = model(X_batch.to(device))
+            loss = criterion(pred, y_batch.to(torch.long).to(device))
+            acc = accuracy_score(y_batch.to(device),torch.argmax(pred,1))
+            losses.append(float(loss))
+            accs.append(float(acc))
+            loss.backward()
+            optimizer.step()
+        print(f'Epoch {epoch+1}: train loss = {round(statistics.mean(losses),3)}, train acc = {round(statistics.mean(accs),3)}')
+    return model
+
+
+
+
+def main():
+    params = yaml.safe_load(open("params.yaml"))["train"]
+    opt_name = params["optimizer"]
+    learning_rate = params["lr"]
+    n_epochs = params["epochs"]
+
+
+    model = nn.Sequential(
+        nn.Linear(5,64),
+        nn.ReLU(),
+        nn.Linear(64,64),
+        nn.ReLU(),
+        nn.Linear(64,32),
+        nn.ReLU(),
+        nn.Linear(32 ,3),
+    )
+
+    if opt_name.upper == "ADAM":
+        opt = torch.optim.Adam(model.parameters(),lr=learning_rate)
+    else:
+        opt = torch.optim.SGD(model.parameters(),lr=learning_rate)
+
+    criterion = torch.nn.CrossEntropyLoss()
+
+    myDs = IrisDatatset('data/prepared/train.csv')
+    train_loader = DataLoader(myDs,batch_size=16,shuffle=False)
+
+    model_tr = train(model, train_loader, opt, criterion, n_epochs)
+
+    torch.save(model_tr.state_dict(),'model.pkl')
+
+if __name__ == "__main__":
+    main()
+
+
